fb8a00ca9c0effc45a4d907cbb47e890a12b7606
rishidas2004 <rishidas24092004@gmail.com>
2025-06-29 22:19:54 +0530

    nullish

---
+++ nullish_coalescing.js
@@ -0,0 +1,14 @@
+//  Nullish Coalescing Operator (??) in JavaScript – Easy Definition:
+
+// The nullish coalescing operator (??) returns the right-hand value only if the left-hand value is null or undefined.
+
+// ✅ Simple Definition:
+// It provides a default value only when the original value is missing (null or undefined), not when it's 0, false, or "".
+
+let name = "rishi";
+
+console.log( name ?? "vips");
+
+let school ;
+
+console.log(school ?? "india");
